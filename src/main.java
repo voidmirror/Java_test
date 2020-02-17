0deb3d680a7eb3d8e2bb8727b1f1c37c5abc8a8c
voidmirror <49756045+voidmirror@users.noreply.github.com>
2020-02-17 11:49:45 +0300

    Update main.java

--- src/main.java
+++ src/main.java
@@ -3,13 +3,33 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         Complexx step = new Complexx(1, 2);
         Complexx stop = new Complexx(3, 4);
 
         Complexx res = new Complexx();
-        res = step.sumComplexx(stop);
-        System.out.println(res.getReal() + " " + res.getImage());
 
+        System.out.println("Multiply:");
+        res = step.multimpyComplexx(stop);
+        System.out.println(res);
+
+        System.out.println("");
+        System.out.println("Divide:");
+        res = step.divideComplexx(stop);
+        System.out.println(res);
+
+        System.out.println("");
+        System.out.println("Sum:");
+        res = step.sumComplexx(stop);
+        //System.out.println(res.getReal() + " " + res.getImage());
+        System.out.println(res);
+
+
+        System.out.println("");
+        System.out.println("Trigonometry:");
+        res.trigonometryView();
+
+        //System.out.println(Math.ceil(0.99999));
 
     }
 }
